9787c6d5f4d0c6895462bfe3b8ebd702d1165b8e
icollectech <icollectiphones@gmail.com>
2026-05-30 18:31:19 -0700

    Delete api directory

--- api/match.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    results: [
-      { name: "iPhone 2G Prototype EVT", match: 95 },
-      { name: "Apple Engineering Sample", match: 88 },
-      { name: "MacBook Prototype Unit", match: 81 }
-    ]
-  })
-}
